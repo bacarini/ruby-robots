0a7c50b900b25efee17c5432ea547fc422984bb4
Bruno Bacarini <bacarini.bruno@gmail.com>
2014-05-24 12:50:31 -0300

    add test to model table

--- spec/lib/table_spec.rb
+++ spec/lib/table_spec.rb
@@ -1,4 +1,10 @@
 require "spec_helper"
 
 describe Table do
+  describe "#in_boundary?" do
+    it { expect(Table.new.in_boundary?(5,5)).to be_true }
+    it { expect(Table.new.in_boundary?(0,0)).to be_true }
+    it { expect(Table.new.in_boundary?(1,2)).to be_false }
+    it { expect(Table.new.in_boundary?(3,4)).to be_false }
+  end
 end
