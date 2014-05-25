fc7a94d4fe4f100cc14aba09479d6d81f418b8ea
Bruno Bacarini <bacarini.bruno@gmail.com>
2014-05-25 13:16:15 -0300

    test thw whole table class

--- spec/lib/table_spec.rb
+++ spec/lib/table_spec.rb
@@ -7,4 +7,28 @@ describe Table do
     it { expect(Table.new.in_boundary?(1,2)).to be_false }
     it { expect(Table.new.in_boundary?(3,4)).to be_false }
   end
+
+  describe "#right_bottom?" do
+    it { expect(Table.new.right_bottom?(5,0)).to be_true }
+    it { expect(Table.new.right_bottom?(0,0)).to be_false }
+    it { expect(Table.new.right_bottom?(0,5)).to be_false }
+  end
+
+  describe "#left_bottom?" do
+    it { expect(Table.new.left_bottom?(0,0)).to be_true }
+    it { expect(Table.new.left_bottom?(0,5)).to be_false }
+    it { expect(Table.new.left_bottom?(4,4)).to be_false }
+  end
+
+  describe "#left_top?" do
+    it { expect(Table.new.left_top?(0,5)).to be_true }
+    it { expect(Table.new.left_top?(5,5)).to be_false }
+    it { expect(Table.new.left_top?(5,0)).to be_false }
+  end
+
+  describe "#right_top?" do
+    it { expect(Table.new.right_top?(5,5)).to be_true }
+    it { expect(Table.new.right_top?(0,5)).to be_false }
+    it { expect(Table.new.right_top?(5,0)).to be_false }
+  end
 end
