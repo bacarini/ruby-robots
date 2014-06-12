062a3928ce14e5713cc83aafb5bd51c6ab9d1d22
Bruno Bacarini <bacarini.bruno@gmail.com>
2014-06-12 13:21:42 -0300

    Interactive Mode
    Taking input from STDIN to run the program interactively.

--- main.rb
+++ main.rb
@@ -1,15 +1,51 @@
 Dir["lib/**/*.rb"].each { |file| load file }
 
-input = File.readlines("input.txt")
-file_pointer =  0
+@robot = Robot.new(Table.new)
 
-robot = Robot.new(Table.new)
+def valid?(answer)
+  answer == "NO" || answer == "YES"
+end
 
-while file_pointer < input.size
+def handle_error(&block)
   begin
-    robot.execute input[file_pointer].chomp
-    file_pointer += 1
+    block.call
   rescue Exception => e
-    raise "Error: #{e.message}. Please check your input and try again!"
+    raise "Error: #{e.message}. Please check your input and try again!\n"+
+    "Valid Commands: [PLACE X,Y,FACING ; LEFT ; RIGHT ; MOVE ; REPORT]"
   end
 end
+
+def by_file
+  input = File.readlines("input.txt")
+  file_pointer =  0
+
+  while file_pointer < input.size
+    handle_error do
+      @robot.execute input[file_pointer].chomp
+      file_pointer += 1
+    end
+  end
+end
+
+def by_command
+  while true
+    handle_error do
+      puts "Please, enter your command or type exit to leave the system:"
+      command = gets.chomp.upcase
+      return if command == "EXIT"
+      @robot.execute command
+    end
+  end
+end
+
+def start
+  puts "The system will proceed to the file input. Would you like to enter your commands manually instead? (yes/no) "
+  answer = gets.chomp.upcase
+  if  valid?(answer)
+    answer == 'YES' ? by_command : by_file
+  else
+    start
+  end
+end
+
+start
